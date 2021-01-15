6ad0631a51266064f888b9748022811f6d4bf42a
Jerrod Styrk <jstyrk@citadel.edu>
2021-01-15 13:33:24 -0800

    Add Flavor Details view/controller

--- PierreBakery/Controllers/FlavorsController.cs
+++ PierreBakery/Controllers/FlavorsController.cs
@@ -11,5 +11,42 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace PierreBakery.Controllers
 {
-    
+    public class FlavorsController : Controller
+    {
+        private readonly PierreBakeryContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public FlavorsController(UserManager<ApplicationUser> userManager, PierreBakeryContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_db.Flavors.ToList());
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Flavor flavor, int TreatId)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            flavor.User = currentUser;
+            _db.Flavors.Add(flavor);
+            if (TreatId != 0)
+            {
+                _db.FlavorTreat.Add(new FlavorTreat() { TreatId = TreatId, FlavorId = flavor.FlavorId});
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
 }
